af5b7bc7d60a08fe54db1675c04da0a73c7f1be6
LIAMSACC <LIAMFAWCETT@HOTMAIL.COM>
2020-07-28 19:17:40 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -19,10 +19,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "🐼 Ellis Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "🐼 Esmé Wins!";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
